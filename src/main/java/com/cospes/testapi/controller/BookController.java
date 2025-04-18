861a1ed461818f341c027d4d269983e57ef7a229
Alessandro Murineddu <alessandro2003.am3@gmail.com>
2025-04-18 16:36:56 +0200

    added book controller

--- src/main/java/com/cospes/testapi/controller/BookController.java
+++ src/main/java/com/cospes/testapi/controller/BookController.java
@@ -22,11 +22,35 @@ public class BookController {
 
     @PostMapping
     public Book create(@RequestBody Book book){
+
         return bookRepository.save(book);
     }
 
     @GetMapping("/id")
     public Book getById(@PathVariable long id){
+
         return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book updateBook (@PathVariable Long id, @RequestBody Book updateBook){
+        return bookRepository.findById(id).map( book -->
+        {
+             book.setTitolo(updateBook.getTitolo());
+             book.setAutore(updateBook.getAutore());
+             return bookRepository.save(book);
+        }).orElse(null);
+
+        //versione alternativa
+        Book updateBook2 = bookRepository.findById(id);
+
+        if(bookFromDB != null){
+            bookFromDB.setAutore(updateBook.getAutore());
+            bookFromDB.setTitolo(updateBook.getTitolo());
+
+            bookRepository.save(bookFromDB);
+        }
+        else{
+            return null;
+        }
     }
 }
